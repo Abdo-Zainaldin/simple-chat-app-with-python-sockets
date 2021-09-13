b647af0a1fe2b88bf286c85179394cc5b0c5a293
zenal <abdozenaldeen1@gmail.com>
2021-09-14 00:59:54 +0300

    first initialize commit

--- server.py
+++ server.py
@@ -14,11 +14,14 @@ server.bind((SERVER,PORT))
 server.listen()
 print(f'[SERVER IS LISTENING] wating for connections...')
 
-# list for clients and ther usernames
+# dict for clients {client:room}
 clients = {}
-userNames = []
 
+# dict for chat rooms {room:[list of clients]}
 chat_rooms = {'0':[]}
+
+# list for clients names
+userNames = []
 
 # disconnecting/deleting clients function
 def disconnect(client):
@@ -27,43 +30,58 @@ def disconnect(client):
     chat_rooms[clients[client]].remove(client)
     clients.pop(client)
 
-# broadcast message
+# broadcast message by the room prameter
 def broadcast(msg,room):
     for client in room:
         client.send(msg.encode(FORMAT))
 
 # handle client
 def handle(client,address):
-    name = list(clients.keys()).index(client)
-    print(f'[HAS BEING HANDLED] {userNames[name]} , {address}')
+    # get name of the client to print in server console
+    usrer_name = userNames[list(clients.keys()).index(client)]
+    print(f'[HAS BEING HANDLED] {usrer_name} , {address}')
+
     while True:
         try:
+            # receive messages from client
             msg = client.recv(HEADER).decode(FORMAT)
             
-            name = list(clients.keys()).index(client)
+            # get name of the client
+            usrer_name = userNames[list(clients.keys()).index(client)]
 
+            # handel the disconnect message
             if msg == DiSCONNECT_MESSAGE:
-                print(f'[HAS LEFT] {userNames[name]}')
+                print(f'[HAS LEFT] {usrer_name}')
                 client.send(DiSCONNECT_MESSAGE.encode(FORMAT))
                 disconnect(client)
                 break
+            # handel the '/' commands
             elif msg.startswith('/'):
                 txtAfterCommand = msg.split()[1]
+
+                # handel the '/room' command
                 if msg.startswith('/room') and len(msg.split()) > 1:
+                    
+                    # try if the specified room existed else create a new one
                     try:
                         chat_rooms[txtAfterCommand]
                     except:
                         chat_rooms[txtAfterCommand] = []
-                    broadcast(f'{userNames[name]} joind!',chat_rooms[txtAfterCommand])
+
+                    # brodcast that there is a cliet has
+                    broadcast(f'{usrer_name} joind!',chat_rooms[txtAfterCommand])
+
+                    # remove the client from the main room '0'
                     chat_rooms[clients[client]].remove(client)
+
+                    # add the clien to the new room
                     clients[client] = txtAfterCommand
                     chat_rooms[clients[client]].append(client)
                 continue
+
+            # broadcast the message to all clients with in the client room
             elif msg:
-                broadcast(f'{userNames[name]}:{msg}',chat_rooms[clients[client]])
-                # print(f'!=\nclient room = {len(chat_rooms[clients[client]])}')
-                # print(f'0 room = {len(chat_rooms["0"])}')
-                # print(f'lols room = {len(chat_rooms["lols"])}')
+                broadcast(f'{usrer_name}:{msg}',chat_rooms[clients[client]])
             else:
                 continue
         except:
@@ -95,10 +113,3 @@ def receive():
 
 receive()
 
-# 1-make rooming seystem
-# by creating alist of rooms like the clients and thir usernames 
-# and maybi create a dict so that evry client must have a value like a room id
-
-# 2-login/authenication system
-
-# 3-add a database sql/mongo-db
